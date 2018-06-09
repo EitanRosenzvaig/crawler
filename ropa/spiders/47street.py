d3bae1a5c01c2520465ef4c65ac530b3f689ee5a
Eitan <eitanrosenzvaig@gmail.com>
2018-06-09 10:21:00 -0300

    47 y Bendito

---
+++ ropa/spiders/47street.py
@@ -0,0 +1,83 @@
+import time
+from scrapy.http import Request
+from datetime import datetime
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+from ropa.items import Item
+
+from pymongo import MongoClient
+
+from text_parser import price_normalize, html_text_normalize
+
+class CuarentiSieteStreet(CrawlSpider):
+    name = 'cuarentisietestreet'
+    allowed_domains = ['47street.com.ar']
+
+    start_urls = ['https://47street.com.ar/invierno18/calzado-aw18.html']
+                
+
+
+    def __init__(self):
+        CrawlSpider.__init__(self)
+        self.verificationErrors = []
+        # self.browser = webdriver.PhantomJS()
+        self.browser = webdriver.Chrome()
+        self.browser.set_page_load_timeout(120)
+        self.connection = MongoClient("localhost", 27017)
+        self.comments = self.connection.ropa.items
+        self.links = self.connection.ropa.links
+
+    rules = [
+        # Rule(LinkExtractor(restrict_xpaths="//a[@class='f-linkNota']"), callback='parse_item', follow=True)
+        # Rule(LinkExtractor(allow_domains=allowed_domains), callback='parse_item', follow=True)
+    ]
+
+    def flaten_array_of_strings(self, array):
+        if len(array) > 0:
+            final_string = array[0]
+            for i in range(1, len(array)-1):
+                final_string += " " + array[i]
+            return(final_string)
+        else:
+            return("")
+
+
+    def parse(self, response):
+        print("------------- Crawling ----------------")
+        self.browser.get(response.url)
+        sel = Selector(text=self.browser.page_source)
+        links = sel.xpath('.//a[@class="product photo product-item-photo"]/@href')
+        for link in links:
+            url_txt = link.extract()
+            if self.links.find_one({"_id": url_txt}) is None:
+                print("------------Found new link: "+str(url_txt))
+                yield Request(url_txt, callback=self.parse_item)
+
+    def parse_item(self, response):
+        if self.links.find_one({"_id": response.url}) is None:
+            print("------------- New Item ----------------")
+            self.browser.get(response.url)
+            time.sleep(2)
+            source = self.browser.page_source
+            sel = Selector(text=source)
+            item = Item()
+            item['created_at'] = datetime.now()
+            item['url'] = response.url
+            item['brand'] = '47street'
+            item['breadcrumb'] = []
+            item['title'] = sel.xpath('.//span[@class="base"]/text()').extract()[0]
+            item['description'] = html_text_normalize(sel.xpath('.//div[@itemprop="description"]/text()').extract())
+            item['code'] = sel.xpath('.//div[@itemprop="sku"]/text()').extract()[0].replace('SKU# ', '')
+            item['price'] = price_normalize(sel.xpath('.//span[@class="price"]/text()').extract()[0])
+            sizes = sel.xpath('.//div[@class="swatch-option text"]/text()').extract()
+            item['sizes'] = sizes
+            item['image_urls'] = sel.xpath('.//div[@class="imagen"]/img/@src').extract()
+            yield item
+            self.links.insert({"_id": response.url})
+        else:
+            print("-------------- OLD -------------")
---
+++ ropa/spiders/benditopie.py
@@ -0,0 +1,84 @@
+import time
+from scrapy.http import Request
+from datetime import datetime
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+from ropa.items import Item
+
+from pymongo import MongoClient
+
+from text_parser import price_normalize, html_text_normalize
+
+class BenditoPie(CrawlSpider):
+    name = 'benditopie'
+    allowed_domains = ['benditopie.com']
+
+    start_urls = ['https://benditopie.com/collections/zapatos-1']
+                
+
+
+    def __init__(self):
+        CrawlSpider.__init__(self)
+        self.verificationErrors = []
+        # self.browser = webdriver.PhantomJS()
+        self.browser = webdriver.Chrome()
+        self.browser.set_page_load_timeout(120)
+        self.connection = MongoClient("localhost", 27017)
+        self.comments = self.connection.ropa.items
+        self.links = self.connection.ropa.links
+
+    rules = [
+        # Rule(LinkExtractor(restrict_xpaths="//a[@class='f-linkNota']"), callback='parse_item', follow=True)
+        # Rule(LinkExtractor(allow_domains=allowed_domains), callback='parse_item', follow=True)
+    ]
+
+    def flaten_array_of_strings(self, array):
+        if len(array) > 0:
+            final_string = array[0]
+            for i in range(1, len(array)-1):
+                final_string += " " + array[i]
+            return(final_string)
+        else:
+            return("")
+
+
+    def parse(self, response):
+        print("------------- Crawling ----------------")
+        self.browser.get(response.url)
+        sel = Selector(text=self.browser.page_source)
+        links = sel.xpath('.//a[@class="product-image view-alt"]/@href')
+        for link in links:
+            url_txt = 'https://benditopie.com/' + link.extract()
+            if self.links.find_one({"_id": url_txt}) is None:
+                print("------------Found new link: "+str(url_txt))
+                yield Request(url_txt, callback=self.parse_item)
+
+    def parse_item(self, response):
+        if self.links.find_one({"_id": response.url}) is None:
+            print("------------- New Item ----------------")
+            self.browser.get(response.url)
+            time.sleep(2)
+            source = self.browser.page_source
+            sel = Selector(text=source)
+            item = Item()
+            item['created_at'] = datetime.now()
+            item['url'] = response.url
+            item['brand'] = 'benditopie'
+            item['breadcrumb'] = []
+            item['title'] = sel.xpath('.//h1[@itemprop="name"]/text()').extract()[0]
+            item['description'] = html_text_normalize(sel.xpath('.//div[@itemprop="description"]//span/text()').extract())
+            item['code'] = ''
+            item['price'] = price_normalize(sel.xpath('.//span[@itemprop="price"]/text()').extract()[0])
+            size_labels = sel.xpath('.//select[@id="ProductSelect-product-template"]/option[not(contains(text(),"gotado"))]/text()').extract()
+            item['sizes'] = [label.strip()[:2] for label in size_labels]
+            image_urls = sel.xpath('.//ul[@id="ProductThumbs-product-template"]/li/a/@href').extract()
+            item['image_urls'] = [url[2:] for url in image_urls]
+            yield item
+            self.links.insert({"_id": response.url})
+        else:
+            print("-------------- OLD -------------")
